5889ca9dc49287ec5bd9821baea178695f1bac45
Noah Spott <ncs5113@psu.edu>
2023-10-25 11:13:47 -0400

    - consolidate style sheets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import './App.css'
 import Upload from './components/Upload'
 import Footer from './components/Footer'
-import Header from './components/header'
+import Header from './components/Header'
 import Xray from './components/Xray'
 
 function App() {
