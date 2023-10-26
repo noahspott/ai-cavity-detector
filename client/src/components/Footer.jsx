e19815b3aee8f80f763707d514191a481670a8e7
Noah Spott <ncs5113@psu.edu>
2023-10-26 13:04:51 -0400

    - footer css - upload css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,16 +4,24 @@ export default function Footer() {
     return(
         <div id='footer-container'>
             <div className='footer-column'>
-                <h4>Title</h4>
-                <p>Description</p>
+                <h3>Disclaimer</h3>
+                <p>The Cavity Detection AI model's results are for informational purposes 
+                    and should not replace professional medical advice.</p>
             </div>
             <div className='footer-column'>
-                <h4>Title</h4>
-                <p>Description</p>
+                <h3>Data</h3>
+                <p>The Cavity Detection machine learning model was trained on 
+                    the <a>DENTEX training set</a></p>
             </div>
             <div className='footer-column'>
-                <h4>Title</h4>
-                <p>Description</p>
+                <h3>Credit</h3>
+                <p>This web application was designed and created by:</p>
+                <ul>
+                    <li>Michael Stanley</li>
+                    <li>Noah Spott</li>
+                    <li>Mason Ticehurst</li>
+                    <li>Keian Kaserman</li>
+                </ul>
             </div>
         </div>
     )
--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -7,7 +7,7 @@ export default function FileUpload() {
                 <img id='upload-tooth-image' src="client/public/upload-tooth.jpg" alt="Upload Image" />
                 <h3>DRAG FILE TO UPLOAD</h3>
             </div>
-            <button>Process</button>
+            <button id='process-button'>Process</button>
         </div>
     )
 }
