d55fdc15c6e4b9d26f800a376935cfd8406fb77a
Noah Spott <ncs5113@psu.edu>
2023-11-04 14:56:53 -0400

    - api call to backend on process button click - backend receives and returns confirmation string - TODO: get backend to return image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Upload from './components/Upload'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -10,10 +11,32 @@ function App() {
   const [userImage, setUserImage] = React.useState(null)
   const [modelOutput, setModelOutput] = React.useState(null)
 
+  const baseURL = 'http://127.0.0.1:5000'
+  const processEndPoint = '/process'
+
   function processButtonClick() {
     console.log("Process Button Clicked!")
 
-    console.log(userImage)
+    // TODO: make button unclickable if no userImage
+
+    if(userImage){
+      console.log(userImage)
+
+      const formData = new FormData();
+      formData.append('file', userImage, userImage.name);
+  
+      axios.post(baseURL + processEndPoint, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data' // Set the content type to multipart form-data
+        }
+      })
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      })
+    }
   }
 
   return (
