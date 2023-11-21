b4e4f2bd136dd555ca46adcfa559605c1e4f8311
KappaJ <masonticehurst@me.com>
2023-11-21 17:34:28 -0500

    Update flask server to respond with prediction data

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS, cross_origin
 import sys
-from io import BytesIO
 from PIL import Image
 # import ML.train_scripts.predict as predict
-from ML.train_scripts.predict import runPrediction
+from ML.predict_scripts.predict import runPrediction
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -12,12 +11,7 @@ CORS(app, supports_credentials=True)
 def process_image(input_image):
     """Processes and returns the xray image"""
     img = Image.open(input_image)
-    
-    
-    # processed_img = img.resize((50, 50))    # for testing w/o ML model
-    processed_img = runPrediction(img)
-
-    return processed_img
+    return runPrediction(img)
 
 @app.route('/process', methods=['POST'])
 @cross_origin()
@@ -32,14 +26,7 @@ def process_request():
         return "No selected file", 400
     
     if file:
-        processed_img = process_image(file)
-
-        # Save the processed image to a byte buffer
-        img_byte_array = BytesIO()
-        processed_img.save(img_byte_array, format='PNG')
-        img_byte_array.seek(0)
-
-        return send_file(img_byte_array, mimetype='image/png')
+        return process_image(file)
     
 
 if __name__ == '__main__':
