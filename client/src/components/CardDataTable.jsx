af8c21762d1811de86759004482aa0d1127aa195
Noah Spott <ncs5113@psu.edu>
2023-11-29 21:44:52 -0500

    - sort tooth cards by tooth number - color condition by boundary box color

--- client/src/components/CardDataTable.jsx
+++ client/src/components/CardDataTable.jsx
@@ -2,19 +2,47 @@ import React from 'react'
 
 export default function CardDataTable({toothData}) {
 
-    const toothCards = toothData.map(tooth => {
+    // Sort tooth data by tooth number
+    let sortedToothData = [...toothData].sort((a, b) => {
+        const toothA = parseInt(a.tooth.replace('Tooth ', ''), 10);
+        const toothB = parseInt(b.tooth.replace('Tooth ', ''), 10);
+      
+        return toothA - toothB;
+    });
+
+    // Create a card for each tooth
+    const toothCards = sortedToothData.map(tooth => {
+
+        let colorClass = 'off-black'
+
+        switch(tooth.disease) {
+            case 'Caries':
+                colorClass = 'caries'
+                break;
+            case 'Impacted':
+                colorClass = 'impacted'
+                break;
+            case 'Deep Caries':
+                colorClass = 'deep-caries'
+                break;
+            case 'Periapical Lesion':
+                colorClass = 'periapical-lesion'
+                break;
+            default:
+                colorClass = 'off-black'
+        }
+
         return(
-            <div className="tooth-card" key={tooth.tooth_number}>
-                <h3>{tooth.tooth}</h3>
-                <p>Condition: {tooth.disease}</p>
-                <p>Confidence: {tooth.confidence}</p>
+            <div className="tooth-card" key={tooth.tooth}>
+                <p className="text-sm caps bold off-black">{tooth.tooth}</p>
+                <p className="text-sm">Condition: <a className={`${colorClass} bold`}>{tooth.disease}</a></p>
+                <p className="text-sm">Confidence: {tooth.confidence.toFixed(2)}</p>
             </div>
         )
     })
 
     return(
         <>
-            <div>{console.log(toothData)}</div>
             <div className="tooth-card-container">
                 {toothCards}
             </div>
